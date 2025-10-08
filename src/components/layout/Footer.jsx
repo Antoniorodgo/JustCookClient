83f1c29da83b08d8badbaff1843b3342e656b907
Alvaro3c <alvaro.cuallado.g@gmail.com>
2025-10-08 21:11:14 +0200

    test

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer>
-    <div className="footer-logo">MiLogo</div>
+      <div className="footer-logo">MiLogo</div>
 
-    <div className="footer-contact">
-      📧 <a href="mailto:correo@ejemplo.com">proyectoIFP@gmail.com</a>
-    </div>
+      <div className="footer-contact">
+        📧 <a href="mailto:correo@ejemplo.com">proyectoIFP@gmail.com</a>
+      </div>
 
-    <div>
-      <a href="#top" className="footer-btn">Volver arriba</a>
-    </div>
-  </footer>
+      <div>
+        <a href="#top" className="footer-btn">Volver arriba</a>
+      </div>
+      <div>
+        Hecho por Antonio, Alvaro y
+      </div>
+    </footer>
   )
 }
