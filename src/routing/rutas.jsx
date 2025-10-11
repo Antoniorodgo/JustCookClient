abb0a573abb91accca8f0a58fcc384a9972bb7bc
Alvaro3c <alvaro.cuallado.g@gmail.com>
2025-10-11 11:02:43 +0200

    first develop commit

--- src/routing/rutas.jsx
+++ src/routing/rutas.jsx
@@ -1,26 +1,22 @@
-import React from "react";  
-import { Routes, Route, BrowserRouter, Navigate, Link} from "react-router-dom";
+import React from "react";
+import { Routes, Route, BrowserRouter, Navigate, Link } from "react-router-dom";
 import { Inicio } from "../pages/inicio";
 import { Ingredientes } from "../pages/Ingredientes";
-import  {MiNevera}  from "../pages/MiNevera";
+import { MiNevera } from "../pages/MiNevera";
 import { MisRecetas } from "../pages/MisRecetas";
 
-export const Rutas =() => {
+export const Rutas = () => {
     return (
         <BrowserRouter>
             {/*Layout*/}
-
             {/*Contenido central y rutas*/}
-
             <section>
                 <Routes>
-                    <Route path="/" element={<Inicio/>}></Route>
-                    <Route path="/MiNevera" element={<MiNevera/>}></Route>
-                    <Route path="/MisRecetas" element={<MisRecetas/>}></Route>
+                    <Route path="/" element={<Inicio />}></Route>
+                    <Route path="/MiNevera" element={<MiNevera />}></Route>
+                    <Route path="/MisRecetas" element={<MisRecetas />}></Route>
                 </Routes>
-
             </section>
-
         </BrowserRouter>
     )
 }
